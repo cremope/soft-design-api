081e0b72f1a0b76b1054d8d3891e83a669b33ca7
novoa.lopez@gmail.com <novoa.lopez@gmail.com>
2024-09-11 23:19:01 -0300

    Adiciona model request e response de product

---
+++ src/main/java/org/example/model/product/ProductRequest.java
@@ -0,0 +1,114 @@
+package org.example.model.product;
+
+public class ProductRequest {
+    private String title;
+    private String description;
+    private int price;
+    private double discountPercentage;
+    private double rating;
+    private int stock;
+    private String brand;
+    private String category;
+    private String thumbnail;
+
+    public ProductRequest(String title, String description, int price, double discountPercentage, double rating, int stock, String brand, String category, String thumbnail) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.discountPercentage = discountPercentage;
+        this.rating = rating;
+        this.stock = stock;
+        this.brand = brand;
+        this.category = category;
+        this.thumbnail = thumbnail;
+    }
+
+    // Getters e Setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"title\": \"" + title + "\"," +
+                "\"description\": \"" + description + "\"," +
+                "\"price\": " + price + "," +
+                "\"discountPercentage\": " + discountPercentage + "," +
+                "\"rating\": " + rating + "," +
+                "\"stock\": " + stock + "," +
+                "\"brand\": \"" + brand + "\"," +
+                "\"category\": \"" + category + "\"," +
+                "\"thumbnail\": \"" + thumbnail + "\"" +
+                "}";
+    }
+}
---
+++ src/main/java/org/example/model/product/ProductResponse.java
@@ -0,0 +1,108 @@
+package org.example.model.product;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ProductResponse {
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("price")
+    private int price;
+    @JsonProperty("discountPercentage")
+    private double discountPercentage;
+    @JsonProperty("rating")
+    private double rating;
+    @JsonProperty("stock")
+    private int stock;
+    @JsonProperty("brand")
+    private String brand;
+    @JsonProperty("category")
+    private String category;
+    @JsonProperty("thumbnail")
+    private String thumbnail;
+
+    // Getters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
